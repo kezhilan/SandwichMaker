f41b44c05c8a3fa15f225b81ee1cfd0a3984bfbb
kezhilan <kezhilan@charlotte.edu>
2026-07-05 18:55:34 -0400

    recipes file added and endpoints in main

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 
 from .models import models, schemas
-from .controllers import orders, sandwiches, resources
+from .controllers import orders, sandwiches, resources, recipes
 from .dependencies.database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -115,3 +115,34 @@ def delete_resource(resource_id: int, db: Session = Depends(get_db)):
     if resources.read_one(db, resource_id) is None:
         raise HTTPException(status_code=404, detail="Resource not found")
     return resources.delete(db, resource_id)
+
+@app.post("/recipes/", response_model=schemas.Recipe, tags=["Recipes"])
+def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
+    return recipes.create(db=db, recipe=recipe)
+
+
+@app.get("/recipes/", response_model=list[schemas.Recipe], tags=["Recipes"])
+def read_recipes(db: Session = Depends(get_db)):
+    return recipes.read_all(db)
+
+
+@app.get("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def read_one_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    recipe = recipes.read_one(db, recipe_id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipe
+
+
+@app.put("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def update_recipe(recipe_id: int, recipe: schemas.RecipeUpdate, db: Session = Depends(get_db)):
+    if recipes.read_one(db, recipe_id) is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipes.update(db, recipe_id, recipe)
+
+
+@app.delete("/recipes/{recipe_id}", tags=["Recipes"])
+def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    if recipes.read_one(db, recipe_id) is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipes.delete(db, recipe_id)
